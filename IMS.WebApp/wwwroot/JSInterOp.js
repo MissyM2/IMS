b35404924b2d3b0de5091fa6c59ec6d9e4937082
maloney_f <missymaloney1@gmail.com>
2023-10-21 17:14:00 -0400

    produce product and jsinterop

---
+++ IMS.WebApp/wwwroot/JSInterOp.js
@@ -0,0 +1,15 @@
+﻿function PreventFormSubmission(formId)
+{
+
+    document.getElementById(`${ formId}`).addEventListener("keydown", function(event) {
+
+        console.log("formId", formId);
+
+        if (event.keyCode == 13) {
+                event.preventDefault();
+            return false;
+        }
+
+    });
+
+}
